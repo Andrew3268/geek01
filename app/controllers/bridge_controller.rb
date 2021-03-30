aa57120208bedda78b9edbb76ce5bbedb42cb710
Your <scarlet3628@likelion.org>
2021-03-31 06:06:26 +0900

    banner

--- app/controllers/bridge_controller.rb
+++ app/controllers/bridge_controller.rb
@@ -7,5 +7,9 @@ class BridgeController < ApplicationController
     @picks = Pick.all.order("created_at DESC")
     @featureds = Featured.all.order("created_at DESC")
   end
+
+  def gram
+    @picks = Pick.all.order("created_at DESC").limit(10)
+  end
   
 end
